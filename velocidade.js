34f23a89a96e391deb9e9731b71dc486e4b6698a
hamiwlzinho1213 <105645553+hamiwlzinho1213@users.noreply.github.com>
2023-10-17 11:48:19 -0300

    Add files via upload

--- velocidade.js
+++ velocidade.js
@@ -1,11 +1,12 @@
+
 const form = document.querySelector("form")
 const resp = document.querySelector("h3")
 
-form = addEventListener("submit" , (e) =>{
+form.addEventListener("submit", (e)=>{
     e.preventDefault()
 
     const permitida = Number(form.inPermitida.value)
-    const condutor = Number(form.inCodutor.value)
+    const condutor = Number(form.inCondutor.value)
     let leve = permitida + (permitida * 0.2)
 
     if(condutor <= permitida){
